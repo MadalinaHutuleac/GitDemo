3255bbf48487793df62b8c78d1e9b69f70d7a81f
ANdrei Hutuleac <hutuleac.andrew@gmail.com>
2019-12-16 23:51:44 +0200

    test

--- src/demo/ApplicationMain.java
+++ src/demo/ApplicationMain.java
@@ -2,6 +2,6 @@ package demo;
 
 public class ApplicationMain {
     public static void main(String [] args){
-        System.out.println("Demo madalinaaaaaa");
+        System.out.println("Demo madalinaaaaaa1");
     }
 }
